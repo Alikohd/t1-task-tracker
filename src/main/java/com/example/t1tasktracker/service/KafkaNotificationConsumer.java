62b6c22b57bae1a1357c476816c3de3090b7a21b
Alekhd <90794297+Alikohd@users.noreply.github.com>
2025-03-03 06:34:03 +0300

    fix: adjust logging for batch processing

--- src/main/java/com/example/t1tasktracker/service/KafkaNotificationConsumer.java
+++ src/main/java/com/example/t1tasktracker/service/KafkaNotificationConsumer.java
@@ -3,10 +3,9 @@ package com.example.t1tasktracker.service;
 import com.example.t1tasktracker.dto.TaskNotificationDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,21 +18,18 @@ public class KafkaNotificationConsumer {
 
     @KafkaListener(containerFactory = "kafkaListenerContainerFactory",
             topics = "${app.kafka.consumer.topic}", groupId = "${app.kafka.consumer.group-id}")
-    public void listenTaskStatusUpdate(List<TaskNotificationDto> messageList,
-                                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-                                       @Header(KafkaHeaders.RECEIVED_KEY) String key,
-                                       Acknowledgment ack) {
-        for (TaskNotificationDto taskNotification : messageList) {
+    public void listenTaskStatusUpdate(List<ConsumerRecord<Long, TaskNotificationDto>> records, Acknowledgment ack) {
+        for (var record : records) {
             try {
-                log.debug("Processing message {} with key {} in topic {}", taskNotification, key, topic);
-                notificationService.notifyTaskStatusUpdate(taskNotification);
+                log.debug("Processing message {} with key {} in topic {}", record.value(), record.key(), record.topic());
+                notificationService.notifyTaskStatusUpdate(record.value());
             } catch (Exception e) {
-                log.error("Failed to process message {} with key {} in topic {}", taskNotification, key, topic, e);
+                log.error("Failed to process message {} with key {} in topic {}", record.value(), record.key(), record.topic(), e);
 //                may sent message to DLQ
             }
         }
 
         ack.acknowledge();
-        log.info("Batch processed, offset commited for topic {}, batchSize {}", topic, messageList.size());
+        log.info("Batch processed, offset commited for topic {}, batchSize {}", records.get(0).topic(), records.size());
     }
 }
